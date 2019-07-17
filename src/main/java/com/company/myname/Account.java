f88af556d5d08754d34914cdc8b71857d1c94c23
zhdaurenkyzy <51210391+zhdaurenkyzy@users.noreply.github.com>
2019-07-18 03:12:30 +0600

    Create Account.java

---
+++ src/main/java/com/company/myname/Account.java
@@ -0,0 +1,27 @@
+package com.company.myname;
+
+public class Account {
+    private String name;
+    private String password;
+
+    public Account(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
